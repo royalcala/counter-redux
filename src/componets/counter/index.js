48cfde012af914bcc0e8bc6f52197e0757fbf431
Roy Alcala Ortiz <alcala.rao@gmail.com>
2021-08-16 17:41:26 -0500

    with redux

---
+++ src/componets/counter/index.js
@@ -0,0 +1,21 @@
+import { deposit, withdraw } from "../../store/amount/action";
+import { connect } from 'react-redux'
+
+const mapStateToProps = (state)=>{
+    return {
+        amount:state.amountReducer.amount 
+    }
+}
+
+const Counter = ({amount,deposit,withdraw}) => {
+
+    return (
+        <div>
+            <h1>{amount}</h1>
+            <button onClick={() => deposit()}>Deposit 10</button>
+            <button onClick={() => withdraw()}>withdraw -10</button>
+        </div>
+    )
+}
+
+export default connect(mapStateToProps, { deposit, withdraw })(Counter)
---
+++ src/store/amount/action.js
@@ -0,0 +1,18 @@
+const deposit = ()=>{
+    return {
+        type:"DEPOSIT_MONEY",
+        paylod:10
+    }
+}
+
+const withdraw = ()=>{
+    return {
+        type:"WITHDRAW_MONEY",
+        paylod:-10
+    }
+}
+
+export{
+    deposit,
+    withdraw
+}
